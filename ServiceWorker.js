3542ddff79c5bd2278b26c930c22e1c553ff2456
Alexander Kravchencko <aliaksandr.krauchanka@innowise.com>
2025-03-05 11:25:51 +0100

    upd

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "DefaultCompany-Telegram_app-0.62";
-const contentToCache = [
-    "Build/telegram-app-build-dev.loader.js",
-    "Build/telegram-app-build-dev.framework.js",
-    "Build/telegram-app-build-dev.data",
-    "Build/telegram-app-build-dev.wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
--- index.js
+++ index.js
@@ -66,7 +66,7 @@ window.addEventListener("load", function ()
     streamingAssetsUrl: "StreamingAssets",
     companyName: "DefaultCompany",
     productName: "Telegram_app",
-    productVersion: "0.62",
+    productVersion: "0.63",
     showBanner: unityShowBanner,
   };
 
